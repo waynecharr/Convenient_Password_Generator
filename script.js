037645d7d949d92dc56488a6178e5deaeebfc1d3
Mona Lisa <waynecharrette@gmail.com>
2023-07-18 16:56:59 -0400

    Function now brings up prompts

--- script.js
+++ script.js
@@ -16,58 +16,71 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//Contains prompts for the window. 
-
-function generatePassword() {
-}
-
-function validateInput() {
-  var lengthCharacters = prompt("How many characters would you like your password to contain?");
-  if (isNaN(lengthCharacters)) {
-    alert("Your value must be a valid number between 8 and 128");
-    return validateInput();
-  } else if (lengthCharacters < 8) {
-    alert("Your password must contain at least 8 characters");
-    return validateInput();
-  } else if (lengthcharacters > 128) {
-    alert("Your password must be no moer than 128 characters");
-    return validateInput();
-  }
-}
-var specialCharacters = window.confirm("Click OK to confirm including special characters.");
-var numbericCharacters = window.confirm("Click OK to confirm including number characters");
-var lowercaseCharacters = window.confirm("Click OK to confirm containing lowercase characters.");
-var uppercaseCharacters = window.confirm("Click OK to confirm containing uppercase characters.");
-
-var length = parseInt(lengthCharacters);
-
-
-function generatePassword() {
-}
-
-var password = prompt("How many characters would you like your password to contain?")
-var number = parseInt(password);
-
+// generates the Lowercase letter(s)
 function createRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-console.log(createRandomLower());
+// generates the Uppercase letter(s)
 function createRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-console.log(createRandomUpper());
-
+// Generates the random number(s)
 function createRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
-console.log(createRandomNumber());
+// Generates the random symbol(s)
 
 function createRandomSymbols() {
-  const symbols = '!@#$%^&*()?><:"}{_+,./;[]=-'
+  const symbols = '!@#$%^&*()?><:"}{_+./;[]=-'
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-console.log(createRandomSymbols());
+function generatePassword() {
+  function setLength() {
+    while (true) {
+        var setLength = prompt("How many characters would you like your password to contain?");
+        if (isNaN(setLength)) {
+            alert("Your value must be a valid number between 8 and 128");
+        } else if (setLength < 8) {
+            alert("Your password must contain at least 8 characters");
+        } else if (setLength > 128) {
+            alert("Your password must be no more than 128 characters");
+        } else {
+            return setLength;
+        }
+    }
+  }
+  
+  var valueLength = setLength();
+  var length = parseInt(valueLength);
+  var symbols = window.confirm("Click OK to confirm including special characters.");
+  var numeric = window.confirm("Click OK to confirm including number characters");
+  var lowercase = window.confirm("Click OK to confirm containing lowercase characters.");
+  var uppercase = window.confirm("Click OK to confirm containing uppercase characters.");
+
+  const password = (length, symbols, numeric, lowercase, uppercase) =>{
+    const promptResults = [
+      ...(symbols ? createRandomSymbols : []),
+      ...(numeric ? createRandomNumber : []),
+      ...(lowercase ? createRandomLower : []),
+      ...(uppercase ? createRandomUpper : [])
+    ];
+    console.log(promptResults);
+  }
+};
+
+
+
+
+const password = (length, symbols, numeric, lowercase, uppercase) =>{
+  const promptResults = [
+    ...(symbols ? createRandomSymbols : []),
+    ...(numeric ? createRandomNumber : []),
+    ...(lowercase ? createRandomLower : []),
+    ...(uppercase ? createRandomUpper : [])
+  ];
+  console.log(promptResults);
+}

7f305232b8c6782cb7e0e126bf170e87b831e34a
Mona Lisa <waynecharrette@gmail.com>
2023-07-19 17:50:35 -0400

    add images to read me

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 
-#Wayne Charrette Comvenient Password Generator
+## Wayne Charrette Comvenient Password Generator
 
 This project is for my third week module. The index.html, the css.stylesheet, and part of the javascript was already provided. The code I am writing out now is just for the remainder.
 
@@ -13,7 +13,20 @@ This project is for my third week module. The index.html, the css.stylesheet, an
 "Logic for prompts defined" - defines the logic for the prompts. Brings back errors when too short, long, or not a number. \
 "Function now brings up prompts" - prompts now propogate when you click the Generate Password button
 "redid random characters into strings, password generates" - The password generator is now working! Need to add code, and keep the password generator from returning a all false for the window.confirms.
+"added notes to the document" added some final notes to the script.js code. I left in the console.log's even though they're technically redundant. 
+"added imgaes to ReadMe" - straight forward, added images
 
 # Resources
 
-For fromCharacterCode in Javascript, super helpful guide - https://www.net-comber.com/charset.html
+- For fromCharacterCode in Javascript, super helpful guide, though I didn't end up using it. - https://www.net-comber.com/charset.html
+- Used a lot of substack for references and helps with code snippets/examples
+- Met with tutor Katie Hobbs who helped me work out part of the code, particularly the for loop. 
+
+# Live Deployment 
+
+https://waynecharr.github.io/Convenient_Password_Generator/
+
+# Images
+
+<img src=images/generated_password.PNG>
+<img src=images/windows_box.PNG>
--- script.js
+++ script.js
@@ -16,41 +16,53 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-
+//variable is a string that pulls a number between 0-9
 var numbersRandom = "1234567890"
+//variable is a string that pulls an upercase letter from A-Z
 var upperLetters = "ABCDEFGHIJKLMOPQRUSTUVWXYZ"
+//variable is a string that pulls a lowercase letter from a-z
 var lowerLetters = "abcdefghijklmnopqrstuvwxyz"
+//vraible is a string that pulls a symbol
 var symbolsRandom = "!@#$%^&*()?><:=-"
-
+//sets a blank characterBank to be filled later
 var characterBank = ""
 
-
+//name of function that causes the generate password to operate
 function generatePassword() {
+  //function for the first prompt that generates the length
   function setLength() {
     while (true) {
+      //prompt asking how long
         var setLength = prompt("How many characters would you like your password to contain?");
         if (isNaN(setLength)) {
             alert("Your value must be a valid number between 8 and 128");
+            //error returned if value is Not a number
         } else if (setLength < 8) {
             alert("Your password must contain at least 8 characters");
+            //error returned if value is less than 8
         } else if (setLength > 128) {
             alert("Your password must be no more than 128 characters");
+            //error returned if value is more than 128
         } else {
             return setLength;
+            //returns the length
         }
     }
   }
-  
+  //converts function to a variable
   var valueLength = setLength();
+  //converts variable from a value to an integer
   var length = parseInt(valueLength);
+  //returns true/false defining special character
   var symbols = window.confirm("Click OK to confirm including special characters.");
+  //returns true/false defining number
   var numeric = window.confirm("Click OK to confirm including number characters");
+  //returns true/false defining lowercase letter
   var lowercase = window.confirm("Click OK to confirm containing lowercase characters.");
+  //returns true false defining uppercase letter
   var uppercase = window.confirm("Click OK to confirm containing uppercase characters.");
 
-  
+
 
   console.log("length",length);
   console.log("symbols",symbols);
@@ -58,38 +70,39 @@ function generatePassword() {
   console.log("lowercase",lowercase);
   console.log("uppercase",uppercase);
 
-
+   //adds symbols to the character bank.
   if (symbols) {
     console.log("before", characterBank);
     characterBank = characterBank + symbolsRandom; //different version
     console.log("after", characterBank);
   }
-
+ //adds numbers to the character bank.
   if (numeric) {
     console.log("before", characterBank);
     characterBank += numbersRandom; //different version
     console.log("after", characterBank);
   }
-
+ // adds lowercase letters to the character bank.
   if (lowercase) {
     console.log("before", characterBank);
     characterBank = characterBank + lowerLetters;
     console.log("after", characterBank);
   }
-
+ // adds uppercase letters to the character bank
   if (uppercase) {
     console.log("before", characterBank);
     characterBank = characterBank + upperLetters;
     console.log("after", characterBank);
   }
-
+ // sets the password to an empty string
   var password = "";
 
-  for (let i = 0; i < length; i++) { //i stands for index 
+  //sets the length of the for loop, by the length variable from the prompt.
+  for (let i = 0; i < length; i++) { 
+    //sets a random variable and the length of the character bank.
     var random = Math.floor(Math.random() * characterBank.length)
-    //console.log(random);
-    //console.log(characterBank[random]);
-
+   
+    //redefines pass as pass + the character bank, with the random variable
     password = password + characterBank[random];
     console.log("password", password);
 
@@ -101,6 +114,5 @@ function generatePassword() {
 
 };
 
-
-
+// the password is generated. :) 
 
